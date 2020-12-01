fb8a31fd7c771042ffb1f59b8c6ea20f5f99d0f5
krkettle <catrarazou@gmail.com>
2020-12-01 03:24:25 +0000

    [fix] 余分なconsole.logを削除

--- src/es/search.js
+++ src/es/search.js
@@ -6,6 +6,5 @@ dotenv.config();
 export const search = async (region, domainName, index, body) => {
   const client = await generateClient(region, domainName);
   const res = await client.search({ index, body });
-  console.log(JSON.stringify(res.body.hits.hits));
   return res;
 };
